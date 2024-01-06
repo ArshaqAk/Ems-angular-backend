eed6219b2f33e7fd6dc748322f98cf7d8a0fa486
ArshaqAk <arshaqakard@gmail.com>
2024-01-06 22:20:21 +0530

    ems-angular-backend

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonServer= require('json-server')
+const empServer= jsonServer.create();
+const middleware= jsonServer.defaults()
+const router = jsonServer.router('db.json')
+const port = 4000 || process.env.PORT
+empServer.use(middleware)
+empServer.use(router)
+
+empServer.listen(port,()=>{
+    console.log('Listening on port '+port);
+})
